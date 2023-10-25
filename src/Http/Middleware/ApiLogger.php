b7f10a23e477d935c7c3738df0e4e88a12d803c6
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2023-10-25 17:08:10 +0300

    fix

--- src/Http/Middleware/ApiLogger.php
+++ src/Http/Middleware/ApiLogger.php
@@ -4,6 +4,7 @@ namespace LaravelEnso\Api\Http\Middleware;
 
 use Closure;
 use LaravelEnso\Api\Enums\Calls;
+use LaravelEnso\Api\Exceptions\Handler;
 use LaravelEnso\Api\Models\Log;
 use LaravelEnso\Helpers\Services\Decimals;
 
@@ -17,13 +18,27 @@ class ApiLogger
     public function terminate($request, $response)
     {
         Log::create([
-            'user_id'  => $request->user()?->id,
-            'url'      => $request->url(),
-            'route'    => $request->route()->getName(),
-            'method'   => $request->method(),
-            'status'   => $response->status(),
-            'type'     => Calls::Inbound,
+            'user_id' => $request->user()?->id,
+            'url' => $request->url(),
+            'route' => $request->route()->getName(),
+            'method' => $request->method(),
+            'status' => $response->status(),
+            'type' => Calls::Inbound,
             'duration' => Decimals::sub(microtime(true), LARAVEL_START),
         ]);
+
+        if ($response->status() !== 200) {
+            $this->report($request, $response);
+        }
+    }
+
+    private function report($request, $response)
+    {
+        $args = [
+            'Incoming Call', $request->url(), $request->all(),
+            $response->status(), 'Api Call Failed',
+        ];
+
+        (new Handler(...$args))->report();
     }
 }
